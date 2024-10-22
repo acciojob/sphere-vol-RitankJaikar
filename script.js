118c7421d780d1d9544ff85a120e89ea68ea42fe
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-22 15:40:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault();
+	const radius= document.querySelector("#radius");
+	const volume= document.querySelector("#volume");
+	if(parseInt(radius.value)<0) {
+		volume.value= NaN;
+		return;
+	}
+	volume.value= (4/3)*(Math.PI)*(radius.value**3);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
